f82c81a8dc954df5fa544fee9cdc490b00209e7c
Kleber Vales <klebervales.dev@gmail.com>
2025-08-18 12:26:48 -0300

    build: initialize project spring boot with java

---
+++ src/main/java/com/example/springactuatorcustomdemo/SpringActuatorCustomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springactuatorcustomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorCustomDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActuatorCustomDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springactuatorcustomdemo/SpringActuatorCustomDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springactuatorcustomdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorCustomDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
